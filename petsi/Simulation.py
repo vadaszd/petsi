6fa5d5ffcad1c134541015b3451966701b77f3ca
Denes Vadasz <vadaszd@users.noreply.github.com>
2020-05-22 20:59:45 +0200

    TransitionIntervalPlugin

--- petsi/Simulation.py
+++ petsi/Simulation.py
@@ -76,7 +76,8 @@ class AutoFirePlugin(
         return self.TransitionObserver(self, t, self._fire_control)
 
     @dataclass(eq=False)
-    class TransitionObserver(Plugins.AbstractTransitionObserver["AutoFirePlugin"]):
+    class TransitionObserver(Plugins.NoopTransitionObserver["AutoFirePlugin"]):
+
         _fire_control: "FireControl"
         _deadline: float = field(default=0.0, init=False)
 
@@ -86,14 +87,73 @@ class AutoFirePlugin(
         def got_disabled(self, ):
             self._fire_control.disable_transition(self._transition)
 
-        def before_firing(self, ): pass
 
-        def after_firing(self, ): pass
+@dataclass(eq=False)
+class Histogram:
+    _bucket_boundaries: Sequence[float]
+    _buckets: List[int] = field(init=False)
+    _bucket_sizes: List[int] = field(init=False)
+    _min_value: float = field(default=0.0, init=False)
+    _max_value: float = field(default=0.0, init=False)
+    _sum_value: float = field(default=0.0, init=False)
+
+    def __post_init__(self):
+        self._buckets = list(repeat(0, len(self._bucket_boundaries) + 1))
+        self._bucket_sizes = [(upper - lower) for (upper, lower) in zip(self._bucket_boundaries[1:],
+                                                                        self._bucket_boundaries)]
+
+    def __iter__(self) -> Iterator[float]:
+        """ Iterate over the buckets of the histogram, yielding the number of items in each bucket"""
+        num_values_total = float(self.num_values)
+        for bucket_value in self._buckets:
+            yield bucket_value / num_values_total
+
+    @property
+    def density(self):
+        for bucket_value, size in zip(self._buckets[1:], self._bucket_sizes):
+            yield bucket_value / size
+
+    def __len__(self):
+        return self.num_values
+
+    def add(self, value: float):
+        """ Increment the count of elements in the bucket value belongs to """
+        i = bisect(self._bucket_boundaries, value)
+        self._buckets[i] += 1
+
+        self._sum_value += value
+
+        if self._min_value > value or self._min_value == 0.0:
+            self._min_value = value
+
+        if self._max_value < value or self._max_value == 0.0:
+            self._max_value = value
+
+    @property
+    def min(self):
+        return self._min_value
+
+    @property
+    def max(self):
+        return self._max_value
+
+    @property
+    def mean(self):
+        return self._sum_value / self.num_values if self.num_values != 0 else 0
+
+    @cached_property
+    def num_values(self):
+        """ :returns The number of items addedd to the histogram.
+
+            Caveat!!! Only call this function after adding all items to the histogram,
+            as the return value is cached and not recalculated for subsequent invocations.
+         """
+        return sum(self._buckets)
 
 
 @dataclass(eq=False)
 class TokenCounterPlugin(
-        Plugins.AbstractPlugin["TokenCounterPlugin.PlaceObserver", NoopTransitionObserver, NoopTokenObserver]):
+        Plugins.AbstractPlugin["TokenCounterPluginPlaceObserver", NoopTransitionObserver, NoopTokenObserver]):
 
     """ A PetSi plugin providing by-place token-count stats
 
@@ -107,12 +167,6 @@ class TokenCounterPlugin(
     # A function returning the current time
     _clock: Clock
 
-    def clear(self):
-        """ Clear the state of the plugin. Removes all data collected during the previous simulation.
-        """
-        for place_observer in self._place_observers.values():
-            place_observer.clear()
-
     def histogram(self, place_name: str) -> Iterator[float]:
         return self._place_observers[place_name].histogram
 
@@ -121,7 +175,8 @@ class TokenCounterPlugin(
 
 
 @dataclass(eq=False)
-class SojournTimePlugin(Plugins.AbstractPlugin):
+class SojournTimePlugin(
+        Plugins.AbstractPlugin[NoopPlaceObserver, NoopTransitionObserver, "SojournTimePluginTokenObserver"]):
     """ A PetSi plugin providing by-place sojourn time stats
 
         The plugin collects the empirical distribution of the
@@ -136,87 +191,24 @@ class SojournTimePlugin(Plugins.AbstractPlugin):
         The bucket is selected based on the cumulative time the token spent at the place during its whole life.
     """
 
-    @dataclass(eq=False)
-    class Histogram:
-        _bucket_boundaries: Sequence[float]
-        _buckets: List[int] = field(init=False)
-        _bucket_sizes: List[int] = field(init=False)
-        _min_value: float = field(default=0.0, init=False)
-        _max_value: float = field(default=0.0, init=False)
-        _sum_value: float = field(default=0.0, init=False)
-
-        def __post_init__(self):
-            self._buckets = list(repeat(0, len(self._bucket_boundaries) + 1))
-            self._bucket_sizes = [(upper - lower) for (upper, lower) in zip(self._bucket_boundaries[1:],
-                                                                            self._bucket_boundaries)]
-
-        def __iter__(self) -> Iterator[float]:
-            """ Iterate over the buckets of the histogram, yielding the number of items in each bucket"""
-            num_values_total = float(self.num_values)
-            for bucket_value in self._buckets:
-                yield bucket_value / num_values_total
-
-        @property
-        def density(self):
-            for bucket_value, size in zip(self._buckets[1:], self._bucket_sizes):
-                yield bucket_value / size
-
-        def __len__(self):
-            return self.num_values
-
-        def add(self, value: float):
-            """ Increment the count of elements in the bucket value belongs to """
-            i = bisect(self._bucket_boundaries, value)
-            self._buckets[i] += 1
-
-            self._sum_value += value
-
-            if self._min_value > value or self._min_value == 0.0:
-                self._min_value = value
-
-            if self._max_value < value or self._max_value == 0.0:
-                self._max_value = value
-
-        @property
-        def min(self):
-            return self._min_value
-
-        @property
-        def max(self):
-            return self._max_value
-
-        @property
-        def mean(self):
-            return self._sum_value / self.num_values if self.num_values != 0 else 0
-
-        @cached_property
-        def num_values(self):
-            """ :returns The number of items addedd to the histogram.
-
-                Caveat!!! Only call this function after adding all items to the histogram,
-                as the return value is cached and not recalculated for subsequent invocations.
-             """
-            return sum(self._buckets)
-
-    # A function returning the current time
     _clock: Clock
 
     bucket_boundaries_per_visit: Sequence[float] = field(default=(), init=False)
     bucket_boundaries_overall: Sequence[float] = field(default=(), init=False)
 
-    _per_visit_histograms: Dict["Structure.TokenType", Histogram] = field(init=False)
+    _per_visit_histograms: Dict[str, Histogram] = field(init=False)
 
     # A s.t. histogram for each place
-    _overall_histograms: Dict["Structure.TokenType", Histogram] = field(init=False)
+    _overall_histograms: Dict["str", Histogram] = field(init=False)
 
-    def clear(self):
+    def reset(self):
         """ Clear the state of the plugin.
 
         Removes all token observers and the data collected during the previous simulation.
         """
         self._per_visit_histograms.clear()
         self._overall_histograms.clear()
-        self._token_observers.clear()
+        super().reset()
 
     def per_visit_histogram(self, place_name) -> Histogram:
         return self._per_visit_histograms[place_name]
@@ -229,17 +221,46 @@ class SojournTimePlugin(Plugins.AbstractPlugin):
         self._overall_histograms = defaultdict(self._new_overall_histogram)
 
     def _new_per_visit_histogram(self):
-        return self.Histogram(self.bucket_boundaries_per_visit)
+        return Histogram(self.bucket_boundaries_per_visit)
 
     def _new_overall_histogram(self):
-        return self.Histogram(self.bucket_boundaries_overall)
+        return Histogram(self.bucket_boundaries_overall)
 
     def token_observer_factory(self, t: "Structure.Token") -> Optional["Plugins.ATokenObserver"]:
         return SojournTimePluginTokenObserver(self, t, self._clock)
 
 
-class TransitionFrequencyPlugin(Plugins.AbstractPlugin):
-    pass
+@dataclass(eq=False)
+class TransitionIntervalPlugin(
+        Plugins.AbstractPlugin[NoopPlaceObserver, "TransitionIntervalPlugin.TransitionObserver", NoopTokenObserver]):
+
+    clock: Clock
+    bucket_boundaries: Sequence[float] = field(default=(), init=False)
+
+    def transition_observer_factory(self, t: "Structure.Transition") -> \
+            Optional["TransitionIntervalPlugin.TransitionObserver"]:
+        return self.TransitionObserver(self, t)
+
+    def firing_interval_histogram(self, transition_name: str) -> Histogram:
+        return self._transition_observers[transition_name].histogram
+
+    @dataclass(eq=False)
+    class TransitionObserver(Plugins.NoopTransitionObserver["TransitionIntervalPlugin"]):
+
+        previous_firing_time: float = field(init=False)
+        histogram: Histogram = field(init=False)
+
+        def __post_init__(self):
+            self.previous_firing_time = self._plugin.clock.read()
+            self.histogram = Histogram(self._plugin.bucket_boundaries)
+
+        def after_firing(self, ):
+            now = self._plugin.clock.read()
+            self.histogram.add(now - self.previous_firing_time)
+            self.previous_firing_time = now
+
+        def reset(self):
+            self.__post_init__()
 
 
 class Simulator:
@@ -251,6 +272,8 @@ class Simulator:
         self._net.register_plugin(self._token_counter)
         self._sojourn_time = SojournTimePlugin("sojourn time plugin", self._auto_fire.clock,)
         self._net.register_plugin(self._sojourn_time)
+        self._transition_interval = TransitionIntervalPlugin("transition interval plugin", self._auto_fire.clock, )
+        self._net.register_plugin(self._transition_interval)
 
     @property
     def net(self) -> "Structure.Net": return self._net
@@ -293,37 +316,45 @@ class Simulator:
 
     def fire_repeatedly(self, count_of_firings: int):
         self._net.reset()
-        self._token_counter.clear()
-        self._sojourn_time.clear()
-        self._auto_fire.reset()
         self._auto_fire.fire_repeatedly(count_of_firings)
 
     @property
-    def bucket_boundaries_overall(self) -> Sequence[float]:
+    def st_bucket_boundaries_overall(self) -> Sequence[float]:
         return self._sojourn_time.bucket_boundaries_overall
 
-    @bucket_boundaries_overall.setter
-    def bucket_boundaries_overall(self, value: Sequence[float]):
+    @st_bucket_boundaries_overall.setter
+    def st_bucket_boundaries_overall(self, value: Sequence[float]):
         self._sojourn_time.bucket_boundaries_overall = value
 
     @property
-    def bucket_boundaries_per_visit(self) -> Sequence[float]:
+    def st_bucket_boundaries_per_visit(self) -> Sequence[float]:
         return self._sojourn_time.bucket_boundaries_per_visit
 
-    @bucket_boundaries_per_visit.setter
-    def bucket_boundaries_per_visit(self, value: Sequence[float]):
+    @st_bucket_boundaries_per_visit.setter
+    def st_bucket_boundaries_per_visit(self, value: Sequence[float]):
         self._sojourn_time.bucket_boundaries_per_visit = value
+
+    @property
+    def transition_interval_bucket_boundaries(self) -> Sequence[float]:
+        return self._transition_interval.bucket_boundaries
+
+    @transition_interval_bucket_boundaries.setter
+    def transition_interval_bucket_boundaries(self, value: Sequence[float]):
+        self._transition_interval.bucket_boundaries = value
 
     def token_count_histogram(self, place_name: str) -> Iterator[float]:
         """ Returns the empirical distribution of the number of tokens at a given place during the simulation.
         """
         return self._token_counter.histogram(place_name)
 
-    def sojourn_time_overall_histogram(self, place_name: str) -> SojournTimePlugin.Histogram:
+    def sojourn_time_overall_histogram(self, place_name: str) -> Histogram:
         """ Returns a histogram of the amount of time a token spends at a given place during its whole life."""
         return self._sojourn_time.overall_histogram(place_name)
 
-    def sojourn_time_per_visit_histogram(self, place_name: str) -> SojournTimePlugin.Histogram:
+    def sojourn_time_per_visit_histogram(self, place_name: str) -> Histogram:
         """ Returns a histogram of the amount of time a token spends at a given place during a visit."""
         return self._sojourn_time.per_visit_histogram(place_name)
 
+    def transition_interval_histogram(self, transition_name: str) -> Histogram:
+        """ Returns a histogram of the amount of time a token spends at a given place during a visit."""
+        return self._transition_interval.firing_interval_histogram(transition_name)
